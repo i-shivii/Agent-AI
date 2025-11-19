baf7cc4442853fff0052fc6cd126cc7366697890
Shivi Chourey <choureyshivi02@gmail.com>
2025-11-19 15:44:52 +0530

    initial commit

---
+++ sample-agent/sample-agent/agent.py
@@ -0,0 +1,14 @@
+import os
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyAztO8x5pfTq3LCHGEvXoEEtgjGbFnBbrw")
+
+model = genai.GenerativeModel("gemini-2.0-flash")
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ["exit", "quit"]:
+        print("Goodbye!")
+        break
+    response = model.generate_content(user_input)
+    print("Gemini:", response.text)
+
